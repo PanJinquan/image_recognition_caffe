275a7bca8a582fe32232e9fd919c51b106bfd8a9
ubuntu <pan_jinquan@163.com>
2018-07-17 14:34:18 +0800

    add files

--- image_recognition_test.py
+++ image_recognition_test.py
@@ -1,10 +1,14 @@
 # -*-coding: utf-8 -*-
-"""
-@project:
-@author:
-@e-mail:
-@date:
-"""
+'''
+  在Caffe中,彩色图像的通道要求是BGR格式，输入数据是float32类型,范围[0,255]
+  [1]caffe的训练/测试prototxt文件,一般在数据层设置:cale:0.00392156885937,即1/255.0,即将数据归一化到[0,1]
+  [2]当输入数据为RGB图像,float32,[0,1],则需要转换:
+    --transformer.set_raw_scale('data',255)       # 缩放至0~255
+    --transformer.set_channel_swap('data',(2,1,0))# 将RGB变换到BGR
+  [3]当输入数据是RGB图像,int8类型,[0,255],则输入数据之前必须乘以*1.0转换为float32
+    --transformer.set_raw_scale('data',1.0)       # 数据不用缩放了
+    --transformer.set_channel_swap('data',(2,1,0))#将RGB变换到BGR
+'''
 
 import caffe
 import numpy as np
@@ -23,7 +27,7 @@ import sys
 #caffenet网络结构描述文件
 deploy_file = 'config/caffenet/deploy.prototxt'
 #训练好的模型
-model_file = 'models/caffenet/caffenet_train_iter_5000.caffemodel'
+model_file = 'models/caffenet/caffenet_train_iter_3600.caffemodel'
 
 # 均值文件(若没有均值文件,则用[104, 117, 123]代替)
 mean_file='mean/image_mean.npy'
@@ -37,7 +41,6 @@ else:
 #caffe.set_device(0)
 # caffe.set_mode_gpu()
 caffe.set_mode_cpu()
-
 
 # 定义网络模型
 net = caffe.Classifier(deploy_file,  # 调用deploy文件
@@ -57,16 +60,16 @@ labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
 for root, dirs, files in os.walk('test_image/'):
     for file in files:
         # 加载要分类的图片
-        image_file = os.path.join(root, file)
-        input_image = caffe.io.load_image(image_file)
+        image_path = os.path.join(root, file)
+        input_image = caffe.io.load_image(image_path)
 
         # 打印图片路径及名称
-        image_path = os.path.join(root, file)
+
         print('******************************************************')
         print(image_path)
 
-        # 预测图片类别
-        prediction = net.predict([input_image])
+        # 预测图片类别,输入数据要求float32 [0,255]
+        prediction = net.predict([input_image])#输入数据float32,[0,1],所以raw_scale=255
         pre_label=prediction[0].argmax()
         print('predicted class:%d-->%s'%(pre_label,labels[pre_label]))
 
